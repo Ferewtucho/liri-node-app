87cda301d431e6149db91c098c02925f8ff29890
Ferew Tucho <ferewtucho@Ferews-MacBook-Pro.local>
2018-10-11 10:49:51 -0400

    hide keys

--- keys.js
+++ keys.js
@@ -1,7 +1,6 @@
 console.log('this is loaded');
-console.log("\r\n");
 
 exports.spotify = {
-  id: "00bcab8ccfc0470da603a431d2638ecc",
-  secret: "bc59420b8f3241a0869625e4f1082460"
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
